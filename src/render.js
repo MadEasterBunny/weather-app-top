4548987f4250a535c739511981c42692feb09765
MadEasterBunny <40764200+MadEasterBunny@users.noreply.github.com>
2026-01-07 22:24:52 +0900

    Add weather icons and logic for displaying them

--- src/render.js
+++ src/render.js
@@ -2,8 +2,15 @@ import { getData } from "./fetchData";
 import { weatherData } from "./processData";
 
 const weather = document.querySelector("#weather");
+const imagesContext = require.context('./icons', false, /\.png$/);
+const images = {};
+imagesContext.keys().forEach((item) => {
+    const name = item.replace('./', '').replace('.png', '');
+    images[name] = imagesContext(item);
+})
 
 export const renderWeather = async (location) => {
+    console.log(images);
     const data = await getData(location);
 
     if(data) {
@@ -13,22 +20,27 @@ export const renderWeather = async (location) => {
 
         weather.innerHTML = "";
 
-        const img = document.createElement("p");
-        img.textContent = `Icon: ${icon}`;
+        const img = document.createElement("img");
+        img.src = images[icon];
         weather.appendChild(img);
 
         const location = document.createElement("h2");
         location.textContent = address;
         weather.appendChild(location);
 
+        //Need to create a check that changes the °C and °F based on which is set
+        //Change the (unitGroup=metric) portion of URL to changed what data is being displayed
         const temperature = document.createElement("p");
         temperature.textContent = `${temp}°C`;
         weather.appendChild(temperature);
 
+        //Add icons for the following two elements (look online for free icons)
         const humid = document.createElement("p");
         humid.textContent = `Humidity: ${humidity}%`;
         weather.appendChild(humid);
 
+        //Need to create a check that changes the km/h and mph based on which is set
+        //Change the (unitGroup=metric) portion of URL to changed what data is being displayed
         const wind = document.createElement("p");
         wind.textContent = `Wind Speed: ${windspeed}km/h`;
         weather.appendChild(wind);
